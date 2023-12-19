e407e4aa1c73400a716d0084ba52f96736f8ceaa
柒木 <2483482026@qq.com>
2023-12-19 19:43:10 +0800

    fix: 修复gateway网关接收POST请求接收不到请求体从而影参数校验的问题 (#7)

---
+++ qi-api-gateway/src/main/java/com/qimu/qiapigateway/CacheBodyGatewayFilter.java
@@ -0,0 +1,55 @@
+package com.qimu.qiapigateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Author: QiMu
+ * @Date: 2023/12/19 07:34:18
+ * @Version: 1.0
+ * @Description: 缓存主体网关筛选器
+ */
+@Component
+public class CacheBodyGatewayFilter implements Ordered, GlobalFilter {
+
+    public static final String CACHE_REQUEST_BODY_OBJECT_KEY = "cachedRequestBodyObject";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        if (exchange.getRequest().getHeaders().getContentType() == null) {
+            return chain.filter(exchange);
+        } else {
+            return DataBufferUtils.join(exchange.getRequest().getBody())
+                    .flatMap(dataBuffer -> {
+                        DataBufferUtils.retain(dataBuffer);
+                        Flux<DataBuffer> cachedFlux = Flux
+                                .defer(() -> Flux.just(dataBuffer.slice(0, dataBuffer.readableByteCount())));
+                        ServerHttpRequest mutatedRequest = new ServerHttpRequestDecorator(
+                                exchange.getRequest()) {
+                            @Override
+                            public Flux<DataBuffer> getBody() {
+                                return cachedFlux;
+                            }
+
+                        };
+                        exchange.getAttributes().put(CACHE_REQUEST_BODY_OBJECT_KEY, cachedFlux);
+
+                        return chain.filter(exchange.mutate().request(mutatedRequest).build());
+                    });
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
