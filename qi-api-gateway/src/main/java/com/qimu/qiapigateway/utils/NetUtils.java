263987abfc088f07e632ad8d48e69b4d3d7c87f2
qimu <2483482026@qq.com>
2024-01-15 17:46:32 +0800

    fix: 补充获取用户ip工具类

---
+++ qi-api-gateway/src/main/java/com/qimu/qiapigateway/utils/NetUtils.java
@@ -0,0 +1,60 @@
+package com.qimu.qiapigateway.utils;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+
+/**
+ * @Author: QiMu
+ * @Date: 2023/09/26 01:08:48
+ * @Version: 1.0
+ * @Description: 网络工具
+ */
+public class NetUtils {
+
+    private static final String IP_UNKNOWN = "unknown";
+    private static final String IP_LOCAL = "127.0.0.1";
+    private static final int IP_LEN = 15;
+
+    /**
+     * 获取客户端真实ip
+     *
+     * @param request request
+     * @return 返回ip
+     */
+    public static String getIp(ServerHttpRequest request) {
+        HttpHeaders headers = request.getHeaders();
+        String ipAddress = headers.getFirst("x-forwarded-for");
+        if (ipAddress == null || ipAddress.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = headers.getFirst("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = headers.getFirst("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = Optional.ofNullable(request.getRemoteAddress())
+                    .map(address -> address.getAddress().getHostAddress())
+                    .orElse("");
+            if (IP_LOCAL.equals(ipAddress)) {
+                // 根据网卡取本机配置的IP
+                try {
+                    InetAddress inet = InetAddress.getLocalHost();
+                    ipAddress = inet.getHostAddress();
+                } catch (UnknownHostException e) {
+                    // ignore
+                }
+            }
+        }
+        // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ipAddress != null && ipAddress.length() > IP_LEN) {
+            int index = ipAddress.indexOf(",");
+            if (index > 0) {
+                ipAddress = ipAddress.substring(0, index);
+            }
+        }
+        return ipAddress;
+    }
+}
